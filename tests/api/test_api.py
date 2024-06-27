4df22aa4f3076701588396c63bf5d9fae05d56bd
Yulia Klymenko <yliaklymenkovladgit@gmail.com>
2024-06-27 18:27:51 +0300

    new files for second project task

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,1 +1,14 @@
-add new file
+import pytest
+
+@pytest.mark.change
+def test_remove_name(user):
+    user.name = ''
+    assert user.name == ''
+
+@pytest.mark.check
+def test_name(user):
+    assert user.name == 'Yuliia'
+
+@pytest.mark.check
+def test_second_name(user):
+    assert user.second_name == 'Klymenko'
